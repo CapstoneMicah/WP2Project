cdb12d17fecbc866cf3bd3411ff439e37cfc8048
Samuel Farina <sfarina@neptune.cs.kent.edu>
2013-05-04 17:26:32 -0400

    add form field files

---
+++ vehicleMake.php
@@ -0,0 +1,19 @@
+<?php
+require_once('./lib/db.php');
+
+$makeResult = $mysqli->query("SELECT vehicleMake.name, vehicleMake.makeID " .
+                             "FROM vehicleMake");
+?>
+
+      <div id="makeDiv">
+        <select id="makeSelect" name="makeChoice" class="vehicleSelection">
+          <option value="" >Make</option>
+          <?php
+            while($make = $makeResult->fetch_assoc()){
+              echo '<option value="'.$make['makeID'].'">'.$make['name'].'</option>';
+            }
+          ?>
+        </select> <br />
+      </div><!-- End makeDiv -->
+
+
